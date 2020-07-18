5a9f0e06b00964a0e8ec21c35dae7d32fe0f1a44
none <viktorxda@users.noreply.github.com>
2020-07-18 21:40:28 +0200

    dts: add C7S/C8 device tree files

--- scripts/drvgen/drvgen.mk
+++ scripts/drvgen/drvgen.mk
@@ -19,7 +19,7 @@ export DRVGEN_OUT
 
 ALL_DRVGEN_FILE := cust.dtsi
 
-DWS_FILE := $(srctree)/$(DRVGEN_PATH)/codegen.dws
+DWS_FILE := $(srctree)/$(DRVGEN_PATH)/$(MTK_PROJECT).dws
 ifneq ($(wildcard $(DWS_FILE)),)
 DRVGEN_FILE_LIST := $(addprefix $(DRVGEN_OUT)/,$(ALL_DRVGEN_FILE))
 else
